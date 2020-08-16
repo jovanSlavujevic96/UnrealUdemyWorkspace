bf0f1f0733a9d81be6e91724635900c37043e3db
jovanSlavujevic96 <jotzilla96@gmail.com>
2020-08-16 11:47:29 +0200

    Added flow chart image & implementing word guessing!

--- BullCowsGame/BullCowGame-starter-kit/Source/BullCowGame/BullCowCartridge.cpp
+++ BullCowsGame/BullCowGame-starter-kit/Source/BullCowGame/BullCowCartridge.cpp
@@ -4,20 +4,54 @@
 void UBullCowCartridge::BeginPlay() // When the game starts
 {
     Super::BeginPlay();
-    PrintLine(TEXT("Hi there!\nWelcome to the Bull Cows Game!"));
-    PrintLine(TEXT("Guess the 4 letter word!")); // Magic Number Remove @caption
-    PrintLine(TEXT("Press <TAB> to type text\nPress <ENTER> to enter text..."));
+    UBullCowCartridge::SetupGame();
 }
 
 void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
 {
-    ClearScreen();
-    static FString HiddenWord = TEXT("cake"); //must use TEXT() macro to wrap string into FString 
-    // Move outside this function @caption
+    if(bGameOver)
+    {
+        ClearScreen();
+        SetupGame();
+        return;
+    }
     if(HiddenWord == Input)
     {
         PrintLine("You have won!");
+        UBullCowCartridge::EndGame();
         return;
     }
-    PrintLine("You have lost!");
+    PrintLine(TEXT("Lost a life!\nLives remaining: %i"), --Lives);
+    if (Lives > 0)
+    {
+        if(Input.Len() != HiddenWord.Len() )
+        {
+            PrintLine(TEXT("Sorry, try guessing again!\nThe Hidden Word is %i lives remaining.\nYou have lost!"), HiddenWord.Len() ); 
+            UBullCowCartridge::EndGame();
+        }
+    }
+    else
+    {
+        PrintLine(TEXT("You have no lives left!"));
+    }
+}
+
+void UBullCowCartridge::SetupGame()
+{
+    PrintLine(TEXT("Welcome to the Bull Cows Game!"));
+ 
+    //must use TEXT() macro to wrap string into FString
+    HiddenWord = TEXT("cakes"); //initialise Hidden word at the beginning
+    Lives = HiddenWord.Len(); //Set Lives
+    bGameOver = false; //Set GameOver varibale
+
+    PrintLine(FString::Printf(TEXT("Guess the %i letter word!"), HiddenWord.Len() )); 
+    PrintLine(TEXT("You have %d Lives"), Lives);    //There can be used both %d or %i for integers
+    PrintLine(TEXT("Press <TAB> so you could type text\nPress <ENTER> to enter your guess...")); // Prompt Player for Guess
+}
+
+void UBullCowCartridge::EndGame()
+{
+    bGameOver = true;
+    PrintLine(TEXT("Press <ENTER> to play again.") );
 }
